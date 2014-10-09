aac24300618cfb116966cedcc56602772c207269
ismail <ismail@dev-fedora.localdomain>
2014-10-09 16:05:15 +0300

    Working exception handler added

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ from os.path import join, getsize
 hash_database=[]
 
 
+class fileobjects():
+    pass
+
 for root, dirs, files in os.walk('/etc', followlinks=False):
     print(root)
     for file_name in files:
         try:
             f=open(root+"/"+file_name,"rb")
+            myline=f.read()
+            myhash=hashlib.sha224(myline).hexdigest()
+            print("\t"+file_name+"\t\t"+myhash+"\t"+str(os.stat(root+"/"+file_name).st_size))
+            hash_database.append([root+"/"+file_name,myhash])
         except:
             pass
-        myline=f.read()
-        #print(myline)
-        #base64.b64encode(myline)
-        myhash=hashlib.sha224(myline).hexdigest()
-        print("\t"+file_name+"\t\t"+myhash)
-        hash_database.append([root+"/"+file_name,myhash])
-        #print(hashlib.sha224(b"Nobody inspects the spammish repetition").hexdigest())
 
 
 #print(hash_database[:])
@@ -27,6 +27,6 @@ for root, dirs, files in os.walk('/etc', followlinks=False):
 for item in hash_database:
     print(item)
 
-hash_dumped=open("hash_dumped","w+")
+hash_dumped=open("hash_dumped","wb+")
 pickle.dump(hash_database,hash_dumped)
 hash_dumped.close()
